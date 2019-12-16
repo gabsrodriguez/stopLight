acf91a7b66e30fb57c5cabf7f575bab8ba554640
gabsrodriguez <gmederos95@gmail.com>
2019-12-16 21:11:05 +0000

    use state

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,15 +1,20 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [light, lightOn] = useState("light red off");
+
+	const changeRed = () => {
+		lightOn("light red");
+	};
+
 	return (
 		<div className="stop-light-container">
-			<div className="light red">Red</div>
-			<div className="light yellow">Yellow</div>
-			<div className="light green">Green</div>
+			<div className={light} onClick={changeRed}>
+				red
+			</div>
+			<div className="light yellow off">Yellow</div>
+			<div className="light green off">Green</div>
 		</div>
 	);
 }
