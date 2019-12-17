e90822b7728950b5126a53cd99938a2ea7bb1f72
gabsrodriguez <gmederos95@gmail.com>
2019-12-17 17:19:40 +0000

    interval

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,28 +2,42 @@ import React, { useState } from "react";
 
 //create your first component
 export function Home() {
-	const [light1, lightOn1] = useState("light red off");
+	const [light1, setLight1] = useState("light red off");
 
 	const changeRed = () => {
 		if (light1 === "light red off") {
-			lightOn1("light red");
-		} else lightOn1("light red off");
+			setLight1("light red");
+		} else setLight1("light red off");
 	};
 
-	const [light2, lightOn2] = useState("light yellow off");
+	const [light2, setLight2] = useState("light yellow off");
 
 	const changeYellow = () => {
 		if (light2 === "light yellow off") {
-			lightOn2("light yellow");
-		} else lightOn2("light yellow off");
+			setLight2("light yellow");
+		} else setLight2("light yellow off");
 	};
 
-	const [light3, lightOn3] = useState("light green off");
+	const [light3, setLight3] = useState("light green off");
 
 	const changeGreen = () => {
 		if (light3 === "light green off") {
-			lightOn3("light green");
-		} else lightOn3("light green off");
+			setLight3("light green");
+		} else setLight3("light green off");
+	};
+
+	function lightOn() {
+		setInterval(function() {
+			setLight1("light red");
+			setLight2("light yellow");
+			setLight3("light green");
+		}, 3000);
+	}
+
+	let lightOff = () => {
+		setLight1("light red off");
+		setLight2("light yellow off");
+		setLight3("light green off");
 	};
 
 	return (
@@ -34,13 +48,19 @@ export function Home() {
 				<div className={light3} onClick={changeGreen} />
 			</div>
 			<div className="buttons">
-				<button type="button" className="btn btn-light">
-					Click
+				<button
+					type="button"
+					className="btn btn-light"
+					onClick={lightOn}>
+					On
 				</button>
 				<button type="button" className="btn btn-light">
 					Timer
 				</button>
-				<button type="button" className="btn btn-light">
+				<button
+					type="button"
+					className="btn btn-light"
+					onClick={lightOff}>
 					Off
 				</button>
 			</div>
